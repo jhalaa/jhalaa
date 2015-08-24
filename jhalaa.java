f36a991fbccfbf85e90c7d640b4f5dd8c1626a26
jhalaa <jhalaachinoy@gmail.com>
2015-08-24 17:47:38 +0530

    2 commit

--- jhalaa.java
+++ jhalaa.java
@@ -5,18 +5,40 @@ import java.util.*;
  */
 public class jhalaa {
     int t =0;
-    int itemname[10];
-    int itemprice[10];
+    String itemname[10];
+    float itemprice[10];
     boolean imported[10];
+    int quantity[10];
+    int i=0;
     void accept()
     {
-        int i=0;
-        Scanner s=new Scanner();
-        System.out.println("tell us the list of items.Press 1 to exit or 0 to continue");
+
+        Scanner s=new Scanner(System.in);
+        System.out.println("tell us about your list");
         while(t==0)
         {
+
             System.out.printf("item name:");
+            itemname[i]=s.nextLine();
+            System.out.printf("item price:");
+            itemprice[i]=s.nextFloat();
+            System.out.printf("item quantity:");
+            quantity[i]=s.nextInt();
+            System.out.printf("is it imported?:");
+            imported[i]=s.nextBoolean();
+            i++;
+            System.out.println("press 0 to continue or 1 to print the bill");
+            t=s.nextInt();
+
+        }
+        }
+    void printdetails()
+    {
+        for(int j=0;j<i;j++)
+        {
+            float totprice;
             
+            System.out.println(quantity[j]+itemname[j]+":"+totprice);
         }
-        }
+    }
 }
