dbd5b47982bdd4506f6e6f4de7b2c44acfc5f8e4
jhalaa <jhalaachinoy@gmail.com>
2015-08-24 17:28:48 +0530

    starting

---
+++ jhalaa.java
@@ -0,0 +1,22 @@
+import com.sun.tools.doclets.internal.toolkit.util.SourceToHTMLConverter;
+import java.util.*;
+/**
+ * Created by jhalaac on 24/08/15.
+ */
+public class jhalaa {
+    int t =0;
+    int itemname[10];
+    int itemprice[10];
+    boolean imported[10];
+    void accept()
+    {
+        int i=0;
+        Scanner s=new Scanner();
+        System.out.println("tell us the list of items.Press 1 to exit or 0 to continue");
+        while(t==0)
+        {
+            System.out.printf("item name:");
+            
+        }
+        }
+}
